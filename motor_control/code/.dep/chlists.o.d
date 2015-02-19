5b171b4b85302d78047632edc8505f0f7a23c35f
Austin Glaser <austin@boulderes.com>
2015-02-19 11:56:24 -0700

    bring in f303 motor driver code

---
+++ motor_control/code/.dep/chlists.o.d
@@ -0,0 +1,110 @@
+build/obj/chlists.o: \
+ /home/austin/git/personal/school/motor_control/chibios/os/kernel/src/chlists.c \
+ /home/austin/git/personal/school/motor_control/chibios/os/kernel/include/ch.h \
+ chconf.h \
+ /home/austin/git/personal/school/motor_control/chibios/os/ports/GCC/ARMCMx/chtypes.h \
+ /usr/lib/gcc/arm-none-eabi/4.9.3/include/stddef.h \
+ /usr/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h \
+ /usr/arm-none-eabi/include/stdint.h \
+ /usr/arm-none-eabi/include/machine/_default_types.h \
+ /usr/arm-none-eabi/include/sys/features.h \
+ /usr/arm-none-eabi/include/sys/_intsup.h \
+ /usr/lib/gcc/arm-none-eabi/4.9.3/include/stdbool.h \
+ /home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chlists.h \
+ /home/austin/git/personal/school/motor_control/chibios/os/ports/GCC/ARMCMx/chcore.h \
+ /home/austin/git/personal/school/motor_control/chibios/os/ports/GCC/ARMCMx/STM32F3xx/cmparams.h \
+ /home/austin/git/personal/school/motor_control/chibios/os/ports/GCC/ARMCMx/chcore_v7m.h \
+ /home/austin/git/personal/school/motor_control/chibios/os/ports/common/ARMCMx/nvic.h \
+ /home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chsys.h \
+ /home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chvt.h \
+ /home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chschd.h \
+ /home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chsem.h \
+ /home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chbsem.h \
+ /home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chmtx.h \
+ /home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chcond.h \
+ /home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chevents.h \
+ /home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chmsg.h \
+ /home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chmboxes.h \
+ /home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chmemcore.h \
+ /home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chheap.h \
+ /home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chmempools.h \
+ /home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chthreads.h \
+ /home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chdynamic.h \
+ /home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chregistry.h \
+ /home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chinline.h \
+ /home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chqueues.h \
+ /home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chstreams.h \
+ /home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chfiles.h \
+ /home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chdebug.h
+
+/home/austin/git/personal/school/motor_control/chibios/os/kernel/include/ch.h:
+
+chconf.h:
+
+/home/austin/git/personal/school/motor_control/chibios/os/ports/GCC/ARMCMx/chtypes.h:
+
+/usr/lib/gcc/arm-none-eabi/4.9.3/include/stddef.h:
+
+/usr/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h:
+
+/usr/arm-none-eabi/include/stdint.h:
+
+/usr/arm-none-eabi/include/machine/_default_types.h:
+
+/usr/arm-none-eabi/include/sys/features.h:
+
+/usr/arm-none-eabi/include/sys/_intsup.h:
+
+/usr/lib/gcc/arm-none-eabi/4.9.3/include/stdbool.h:
+
+/home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chlists.h:
+
+/home/austin/git/personal/school/motor_control/chibios/os/ports/GCC/ARMCMx/chcore.h:
+
+/home/austin/git/personal/school/motor_control/chibios/os/ports/GCC/ARMCMx/STM32F3xx/cmparams.h:
+
+/home/austin/git/personal/school/motor_control/chibios/os/ports/GCC/ARMCMx/chcore_v7m.h:
+
+/home/austin/git/personal/school/motor_control/chibios/os/ports/common/ARMCMx/nvic.h:
+
+/home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chsys.h:
+
+/home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chvt.h:
+
+/home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chschd.h:
+
+/home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chsem.h:
+
+/home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chbsem.h:
+
+/home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chmtx.h:
+
+/home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chcond.h:
+
+/home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chevents.h:
+
+/home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chmsg.h:
+
+/home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chmboxes.h:
+
+/home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chmemcore.h:
+
+/home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chheap.h:
+
+/home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chmempools.h:
+
+/home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chthreads.h:
+
+/home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chdynamic.h:
+
+/home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chregistry.h:
+
+/home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chinline.h:
+
+/home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chqueues.h:
+
+/home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chstreams.h:
+
+/home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chfiles.h:
+
+/home/austin/git/personal/school/motor_control/chibios/os/kernel/include/chdebug.h:
---
+++ motor_control/code/.dep/crt0.o.d
@@ -0,0 +1,17 @@
+build/obj/crt0.o: \
+ /home/austin/git/personal/school/motor_control/chibios/os/ports/GCC/ARMCMx/crt0.c \
+ /usr/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h \
+ /usr/arm-none-eabi/include/stdint.h \
+ /usr/arm-none-eabi/include/machine/_default_types.h \
+ /usr/arm-none-eabi/include/sys/features.h \
+ /usr/arm-none-eabi/include/sys/_intsup.h
+
+/usr/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h:
+
+/usr/arm-none-eabi/include/stdint.h:
+
+/usr/arm-none-eabi/include/machine/_default_types.h:
+
+/usr/arm-none-eabi/include/sys/features.h:
+
+/usr/arm-none-eabi/include/sys/_intsup.h:
